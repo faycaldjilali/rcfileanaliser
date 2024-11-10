399f29ec030f5c3699f1558328084d6ddad87fca
faycaldjilali <129238896+faycaldjilali@users.noreply.github.com>
2024-11-10 23:00:50 +0000

    Update setup.py
    d

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ setup(
     author='faycal djilali',
     author_email='djilalifaycal97@gmail.com',
     install_requires=[
-        "cohere",
         "streamlit",
-        "python-dotenv",
-        "PyPDF2"
+        "PyPDF2==3.0.1",
+        "cohere==5.11.1",
+        "openpyxl==3.1.5",
+        "python-dotenv==1.0.1",
+        "python-docx==1.1.2",
+
+
+        "reportlab"
     ],
     packages=find_packages(),
 )
