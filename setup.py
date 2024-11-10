0b5f9a0c74680cc1ac9e442414f7265b979b8517
faycaldjilali <129238896+faycaldjilali@users.noreply.github.com>
2024-11-10 22:58:24 +0000

    Update setup.py
    d

--- setup.py
+++ setup.py
@@ -1,42 +1,15 @@
-from setuptools import setup, find_packages
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+from setuptools import find_packages, setup
 
 setup(
-    name="rc-file-analiser",
-    version="0.0.1",
-    author="Faycal Djilali",
-    author_email="faycal.djilali6@gmail.com",
-    description="A tool for analyzing and processing various file types",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/faycaldjilali/rcfileanaliser",
-    packages=find_packages(include=['src', 'src.*']),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    name='rc_file_analiser',
+    version='0.0.1',
+    author='faycal djilali',
+    author_email='djilalifaycal97@gmail.com',
+    install_requires=[
+        "cohere",
+        "streamlit",
+        "python-dotenv",
+        "PyPDF2"
     ],
-    python_requires=">=3.8",
-    install_requires=requirements,
-    include_package_data=True,
-    package_data={
-        'rcfileanaliser': ['*'],
-        'src': ['*'],
-        'src.utils': ['*'],
-        'src.models': ['*'],
-        'src.data': ['*'],
-    },
-    entry_points={
-        'console_scripts': [
-            'rcfileanaliser=src.app:main',
-        ],
-    },
+    packages=find_packages(),
 )
